0606f140db47292053cc1d1aef63d83e087aafd5
Joshuaamm7 <joshhw921222@gmail.com>
2026-04-17 16:30:59 -0700

    s2

--- global.js
+++ global.js
@@ -3,3 +3,15 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+
+let currentLink = navLinks.find((a) => {
+  let linkPath = a.pathname.replace(/index\.html$/, "");
+  let pagePath = location.pathname.replace(/index\.html$/, "");
+  return a.host === location.host && linkPath === pagePath;
+});
+
+if (currentLink) {
+  currentLink.classList.add("current");
+}
