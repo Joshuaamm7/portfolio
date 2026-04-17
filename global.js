0c8f72433437069d750b9adbaa09b045fc233f52
Joshuaamm7 <joshhw921222@gmail.com>
2026-04-17 16:51:18 -0700

    s3

--- global.js
+++ global.js
@@ -1,15 +1,45 @@
-console.log('IT’S ALIVE!');
+console.log("IT’S ALIVE!");
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/";
 
-let currentLink = navLinks.find((a) => {
-  return location.pathname.endsWith(a.getAttribute("href"));
-});
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume/", title: "Resume" },
+  { url: "https://github.com/joshuaamm7", title: "GitHub" },
+];
 
-if (currentLink) {
-  currentLink.classList.add("current");
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith("http") ? BASE_PATH + url : url;
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+
+  let linkPath = a.pathname.replace(/index\.html$/, "");
+  let pagePath = location.pathname.replace(/index\.html$/, "");
+
+  if (a.host === location.host && linkPath === pagePath) {
+    a.classList.add("current");
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
 }
