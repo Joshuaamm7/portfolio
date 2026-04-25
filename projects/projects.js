073185bf8c5e3288065ba306519879281f5a3074
Joshuaamm7 <joshhw921222@gmail.com>
2026-04-24 20:50:02 -0700

    fix6

--- projects/projects.js
+++ projects/projects.js
@@ -3,6 +3,12 @@ import { fetchJSON, renderProjects } from '../global.js';
 // Fetch data
 const projects = await fetchJSON('../lib/projects.json');
 
+projects.forEach(project => {
+  if (!project.image.startsWith('http')) {
+    project.image = `../${project.image}`;
+  }
+});
+
 // Select elements
 const projectsContainer = document.querySelector('.projects');
 const titleElement = document.querySelector('.projects-title');
