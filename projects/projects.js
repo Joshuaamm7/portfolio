3c5f07f8ef7f24758b5a4f07551c02ef7ebd9ca9
Joshuaamm7 <joshhw921222@gmail.com>
2026-05-01 22:41:44 -0700

    1.4

--- projects/projects.js
+++ projects/projects.js
@@ -14,3 +14,22 @@ renderProjects(projects, projectsContainer, 'h2');
 
 // Update project count
 titleElement.textContent = `Projects (${projects.length})`;
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2];
+
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = ['gold', 'purple'];
+
+arcs.forEach((arc, idx) => {
+  d3.select('#projects-pie-plot')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]);
+});
